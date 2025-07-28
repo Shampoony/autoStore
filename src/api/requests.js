d17344eb66652ef1e51087ed08a2c15fea16035d
Shampoony <itnewsbgn@gmail.com>
2025-07-28 10:55:53 +0300

    changed link

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,8 +1,8 @@
 import { accessToken } from './auth'
 import { useRouter } from 'vue-router'
 
-const domain = `http://api.rcarentacar.com/`
-const domainDev = `http://37.252.17.98/`
+const domain = `https://api.rcarentacar.com/`
+const domainDev = `https://37.252.17.98/`
 const router = useRouter()
 
 // Общая функция для всех типов запросов
